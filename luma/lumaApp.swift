55655acba47e952931f2e289e57b4ef91948a808
Juan Carlos Velasco <jcarlosvelasco14@gmail.com>
2024-11-12 15:04:11 +0100

    Initial Commit

---
+++ luma/lumaApp.swift
@@ -0,0 +1,17 @@
+//
+//  lumaApp.swift
+//  luma
+//
+//  Created by Juan Carlos Velasco on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct lumaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
